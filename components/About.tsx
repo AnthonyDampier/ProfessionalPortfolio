1796cf7f47de697ec30590d108ffec1c44ea108e
AnthonyDampier <Dampieranthony2@gmail.com>
2023-01-22 13:29:57 -0600

    About responsiveness adjusted

--- components/About.tsx
+++ components/About.tsx
@@ -1,23 +1,24 @@
 export default function(){
     return(
-        <section className={`mt-40 mb-60 text-center text-lg md:pl-0 md:text-left`}>
-            <h1 className='drop-shadow-lg shadow-black font-bold text-white leading-tight tracking-tighter md:text-4xl mb-1 flex justify-center'>
+        <section className={`mt-40 mb-60 text-center text-lg md:pl-0 bg-black`}>
+            <h1 className='drop-shadow-lg shadow-black font-bold text-white leading-tight tracking-tighter md:text-4xl sm:text-2xl mb-1 flex justify-center'>
                 Hi, I'm Anthony Dampier
             </h1>
-            <h4 className='drop-shadow-lg shadow-black  text-zinc-400 leading-tight tracking-tighter md:text-4l mb-1 flex justify-center'>
+            <h4 className='drop-shadow-lg shadow-black text-zinc-400 leading-tight tracking-tighter md:text-4l mb-1 flex justify-center'>
                 Full Stack Web Developer  
             </h4>
-            <h4 className='font-semibold  text-white leading-tight tracking-tighter md:text-4l text-6xl mt-10 mb-0 text-center'>
+            <p className='font-semibold  text-white leading-tight tracking-tighter xl:text-4xl md:text-xl sm:text-base flex justify-center'>
                 Turning 
-                <span className="z-10 drop-shadow-lg shadow-white font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-200 to-pink-600 ">
+                <div className="z-10 drop-shadow-lg shadow-white font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-200 to-pink-600">
                 &nbsp;ideas&nbsp;
-                </span>
+                </div>
                 into real life
-                <span className="z-10 drop-shadow-lg shadow-white font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-600">
+                <div className="z-10 drop-shadow-lg shadow-white font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-purple-600">
                 &nbsp;products&nbsp;
-                </span>
+                </div>
                 is my calling.  
-            </h4>
+            </p>
+            
         </section>
         
 
